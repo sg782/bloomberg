6793ee55493d99173081de71a8d52b6326092b93
sg782 <sullivanpgleason@gmail.com>
2025-03-31 14:16:34 -0500

    simple autogeneratred api test

---
+++ blpapi_test/bloomberg_api_test.py
@@ -0,0 +1,29 @@
+import blpapi
+from blpapi import SessionOptions, Session
+
+options = SessionOptions()
+options.setServerHost('localhost')
+options.setServerPort(8194)
+
+session = Session(options)
+if not session.start():
+    print("Failed to start session.")
+    exit()
+
+if not session.openService("//blp/refdata"):
+    print("Failed to open service.")
+    exit()
+
+service = session.getService("//blp/refdata")
+request = service.createRequest("ReferenceDataRequest")
+request.getElement("securities").appendValue("AAPL US Equity")
+request.getElement("fields").appendValue("PX_LAST")
+
+session.sendRequest(request)
+
+while True:
+    ev = session.nextEvent()
+    for msg in ev:
+        print(msg)
+    if ev.eventType() == blpapi.Event.RESPONSE:
+        break
