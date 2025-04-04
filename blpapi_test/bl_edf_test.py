9a345d401950c69227e34b7d1b9af6c5454ba563
Gleason <spgleason1s@semo.edu>
2025-04-04 15:32:10 -0500

    found a github repo that seems like it may help

--- blpapi_test/bl_edf_test.py
+++ blpapi_test/bl_edf_test.py
@@ -2,16 +2,20 @@ import blpapi
 from blpapi import SessionOptions, Session
 
 def process_event(event):
+    print(event.eventType())
     for msg in event:
-        if msg.hasElement("messageText"):
-            print("Headline:", msg.getElementAsString("messageText"))
+        print(msg)
+        # if msg.hasElement("messageText"):
+        #     print("Headline:", msg.getElementAsString("messageText"))
 
 def main():
+    print("introduction")
     options = SessionOptions()
     options.setServerHost('localhost')  # or Bloomberg Server IP
     options.setServerPort(8194)
 
     session = Session(options)
+    print("afters ession")
 
     if not session.start():
         print("Failed to start session.")
@@ -27,18 +31,25 @@ def main():
     print("subs: ", subscriptions)
 
     # Subscribe to all headlines – customize as needed
-    subscriptions.add("AAPL",  # News headline source
-                      "storyId, messageText",
-                      "",
-                      blpapi.CorrelationId("news_sub"))
+    # subscriptions.add("AAPL",  # News headline source
+    #                   "storyId, messageText",
+    #                   "",
+    #                   blpapi.CorrelationId("news_sub"))
+    subscriptions.add("AAPL")
 
     session.subscribe(subscriptions)
 
+    print("after sub")
     while True:
-        event = session.nextEvent()
-        if event.eventType() in (blpapi.Event.SUBSCRIPTION_DATA,
-                                 blpapi.Event.SUBSCRIPTION_STATUS):
-            process_event(event)
+        print("inside loop")
+        try:
+            event = session.nextEvent()
+            print(event)
+            if event.eventType() in (blpapi.Event.SUBSCRIPTION_DATA,
+                                    blpapi.Event.SUBSCRIPTION_STATUS):
+                process_event(event)
+        except:
+            print("Error while processing")
 
 if __name__ == "__main__":
     main()
